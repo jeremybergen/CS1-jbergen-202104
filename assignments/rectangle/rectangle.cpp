231bb1d4213481e4fbea2327e37cd6661d0cfcc3
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-02-10 09:26:58 -0700

    completed step 1

--- assignments/rectangle/rectangle.cpp
+++ assignments/rectangle/rectangle.cpp
@@ -10,3 +10,22 @@ Step 2: calculate area = side1 * side2
 Step 3: calculate perimeter = 2 * side1 + 2 * side2
 Step 4: Print out area and perimeter
 */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float side1, side2;
+    //Step 1
+    cout << "Please enter side1 and side2 separated by a space: ";
+    cin >> side1 >> side2;
+    cout << "You entered " 
+         << side1 << " for side1 and "
+         << side2 << " for side2." << endl;
+    //Step 2
+
+    //Step 3
+
+    //Step 4
+    return 0;
+}
