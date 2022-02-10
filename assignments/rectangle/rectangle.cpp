86f15318a1e31ab4933ac99566c8329928cf80ff
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-02-10 09:20:42 -0700

    Initial rectangle program. comments and algorithm at top

---
+++ assignments/rectangle/rectangle.cpp
@@ -0,0 +1,12 @@
+/*
+Name: Jeremy Bergen
+Date: 02/10/22
+
+This program will calculate the 
+area and perimeter of a rectangle
+
+Step 1: prompt the user for side1 and side2
+Step 2: calculate area = side1 * side2
+Step 3: calculate perimeter = 2 * side1 + 2 * side2
+Step 4: Print out area and perimeter
+*/
