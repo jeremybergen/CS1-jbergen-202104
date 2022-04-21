299cae7bfe230c0d9cbc0bbd05969d294cd5f9bb
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-04-21 09:49:46 -0600

    Thursday

--- lecture_notes/structs.cpp
+++ lecture_notes/structs.cpp
@@ -1,73 +1,102 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
-// struct Person {
-//     int student_id;
-//     string lastname;
-//     string firstname;
-//     char mi;
-//     float grade;
-// };
+namespace myCircle {
+    // template<class T1, class T2>
+    struct Circle {
+        float radius;
+        float x, y;
 
-struct Point {
-    float x;
-    float y;
+        double area;
+        double perimeter;
 
-    void myfunction(string name) {
-        cout << "Hello " << name << endl;
-    }
-};
+        int num[10];
+        vector<int> num2;
+        Circle *newcircle;
+    };
+}
+
+// namespace yourCircle {
+//     template<class T1, class T2>
+//     struct Circle {
+//         T1 diameter;
+//         T1 x, y;
+
+//         T2 volume;
+//         T2 perimeter;
+
+//         int num[10];
+
+//     };
+// }
+// void calc_circle(Circle&);
 
 int main(int argc, char *argv[]) {
-    Point origin = {0, 0};
-    Point pt1, pt2, pt3;
+    size_t arr_size = 10;
+    myCircle::Circle circle_array[arr_size];
+    // Circle<int, double> *circle_array = new Circle<int, double>[arr_size];
+    // vector< Circle<int, double> > circle_array;
 
-    pt1 = {4, 2};
-    
-    pt2 = Point({-4, -2});
-    
-    pt3.x = 42;
-    pt3.y = 24;
+    myCircle::Circle circle1;
+    myCircle::Circle c2 = {24, 0, 0};
+    myCircle::Circle *c3 = new myCircle::Circle({2, 4, 5});
 
-    char ch;
-    double distance;
+    // yourCircle::Circle<int, double> yc1;
 
-    pt1.myfunction("Jeremy");
+    // yc1.diameter = 84;
 
-    // cout << "Please enter a point in the format (x, y): ";
-    // cin >> ch >> pt1.x >> ch >> pt1.y >> ch;
+    circle1.radius = 42.5;
+    circle1.x = 2;
+    circle1.y = 4;
+    circle1.num[0] = 42;
+    circle1.num[1] = 24;
+    circle1.num2.push_back(42);
+    circle1.num2.push_back(84);
 
-    // cout << "Please enter a point in the format (x, y): ";
-    // cin >> ch >> pt2.x >> ch >> pt2.y >> ch;
+    c3->num2.push_back(42);
 
-    distance = sqrt(pow((pt1.x - pt2.x), 2) + pow((pt1.y - pt2.y), 2));
+    circle_array[0] = circle1;
+    circle_array[1] = c2;
+    circle_array[2] = *c3;
 
-    // cout << "The distance between (" << origin.x << ", " << origin.y << ") and "
-    //      << "(" << pt1.x << ", " << pt1.y << ") is: " << distance << endl;
-    printf("The distance between (%f, %f) and (%f, %f) is: %f\n", pt2.x, pt2.y, pt1.x, pt1.y, distance);
+    // circle_array.push_back(circle1);
+    // circle_array.push_back(c2);
+    // circle_array.push_back(*c3);
 
+    for (size_t i = 0; i < 3; i++) {
+        cout << i << ": " << circle_array[i].radius << endl;
+    }
+    // cout << circle1.radius << ":" << circle1.x << ":" << circle1.y << endl;
 
+    // c3->radius = 2;
+    // c3->x = 4;
+    // c3->y = 5;
+
+    // calc_circle(circle1);
+    // calc_circle(c2);
+    // calc_circle(*c3);
+    // circle1.area = M_PI * pow(circle1.radius, 2);
+    // cout << "The circle at point (" << circle1.x << ", " << circle1.y << ") with radius "
+    //      << circle1.radius << " has an area of: " << circle1.area << endl;
+    // printf("The circle at point (%f, %f) with radius %f has an area of: %f\n", c3->x, c3->y, c3->radius, c3->area);
+    // printf("The circle at point (%f, %f) with radius %f has an area of: %f\n", circle1.x, circle1.y, circle1.radius, circle1.area);
+    // printf("The circle at point (%f, %f) with radius %f has an area of: %f\n", c2.x, c2.y, c2.radius, c2.area);
+    // cout << "Memory address: " << &circle1 << endl;
+    // cout << "Memory address circle1.radius: " << &circle1.radius << endl;
+    // cout << "Memory address circle1.x: " << &circle1.x << endl;
+    // cout << "Memory address circle1.y: " << &circle1.y << endl;
+    // cout << "Memory address circle1.area: " << &circle1.area << endl;
+
+    // delete[] circle_array;
+    delete c3;
     return 0;
 }
 
-    // Person student1;
-    // Person student2 = {24, "Cochran", "David", 'A'};
-
-    // student1.firstname = "Jeremy";
-    // student1.student_id = 42;
-    // student1.lastname = "Bergen";
-
-    // student1.mi = 'J';
-    // student1.grade = 3.8;
-
-    // cout << "The student id is: " << student1.student_id << endl;
-    // cout << "Your name is: " 
-    //      << student1.firstname << " " << student1.mi << " " << student1.lastname << endl;
-    // cout << "The student grade is: " << student1.grade << endl;
-
-    // cout << "student2 id is: " << student2.student_id << endl;
-    // cout << "student2 name is: " 
-    //      << student2.firstname << " " << student2.mi << " " << student2.lastname << endl;
+// void calc_circle(Circle &c) {
+//     c.area = M_PI * pow(c.radius, 2);
+//     c.perimeter = 2 * M_PI * c.radius;
+// }
